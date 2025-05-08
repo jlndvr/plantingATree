7e1dd1f911e6cebaf040ae78bfe1c732cf8b2e8b
Josué Obed Landaverde <129899297+jlndvr@users.noreply.github.com>
2025-05-08 14:50:58 -0500

    Create Tree.java

---
+++ Tree.java
@@ -0,0 +1,15 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+    // Info
+    System.out.println("Hello: I know JAVA OOP but I want to advance my knowledge in Java and have the courses to prove it.");
+    System.out.println("Project goal: Just do it");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println("*****");
+    System.out.println("* * *");
+    System.out.println("  *  ");
+    System.out.println("  *   ");
+
+  }
+}
